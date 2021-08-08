6dcd519dde6e4edc4dfc34a77f4c802095713ff5
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-08-08 19:23:54 +0700

    🔒 Add micro-podcast-svelte to cors

--- main.go
+++ main.go
@@ -24,17 +24,15 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.Use(cors.Default())
+	// router.Use(cors.Default())
 
-	// router.Use(cors.New(cors.Config{
-	// 	// AllowOrigins:     []string{"http://localhost:8080"},
-	// 	AllowAllOrigins:  true,
-	// 	AllowMethods:     []string{"POST", "OPTIONS"},
-	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Content-Length", "Accept", "Accept-Encoding"},
-	// 	ExposeHeaders:    []string{"Content-Length"},
-	// 	AllowCredentials: true,
-	// 	// MaxAge: 12 * time.Hour,
-	// }))
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://micro-podcast-svelte.vercel.app/"},
+		AllowMethods:     []string{"POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Content-Length", "Accept", "Accept-Encoding"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	router.POST("/rss", func(c *gin.Context) {
 		var rss Rss
@@ -43,15 +41,27 @@ func main() {
 		fp := gofeed.NewParser()
 		feed, _ := fp.ParseURL(rss.Url)
 
-		c.JSON(http.StatusOK, gin.H{
-			"title":       feed.Title,
-			"description": feed.Description,
-			"author":      feed.Author,
-			"authors":     feed.Authors,
-			"update":      feed.Updated,
-			"image":       feed.Image,
-			"items":       feed.Items,
-		})
+		if len(feed.Items) > 25 {
+			c.JSON(http.StatusOK, gin.H{
+				"title":       feed.Title,
+				"description": feed.Description,
+				"author":      feed.Author,
+				"authors":     feed.Authors,
+				"update":      feed.Updated,
+				"image":       feed.Image,
+				"items":       feed.Items[0:25],
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"title":       feed.Title,
+				"description": feed.Description,
+				"author":      feed.Author,
+				"authors":     feed.Authors,
+				"update":      feed.Updated,
+				"image":       feed.Image,
+				"items":       feed.Items,
+			})
+		}
 	})
 
 	router.Run(":" + port)
